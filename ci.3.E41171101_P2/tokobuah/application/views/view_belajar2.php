972192460491181072ab7733fceed0fd7ad6d700
mochammadRozyAndreanSyah <44630729+mochammadRozyAndreanSyah@users.noreply.github.com>
2019-03-19 20:10:53 +0700

    toko buah

---
+++ ci.3.E41171101_P2/tokobuah/application/views/view_belajar2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<title>Cara Membuat Heading dengan Helper HTML Codeigniter</title>
+</head>
+<body>
+	<?php 
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",1);
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",2);
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",3);
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",4);
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",5);
+	echo heading("Helper HTML CodeIgniter | MalasNgoding.com",6);
+	?>
+</body>
+</html>
