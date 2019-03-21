b8bd4ff3251ca7e6dc242ed0966977be1c3e4078
Rangga Triana Putra <44762808+ranggatriana@users.noreply.github.com>
2019-03-21 09:41:41 +0700

    sum
    database 2

--- galon (3).sql
+++ galon (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2019 at 03:19 AM
+-- Generation Time: Mar 21, 2019 at 03:39 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
