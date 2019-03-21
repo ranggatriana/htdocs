8f38cbf6a0a3cba8f9ffdd97d5fc38d249dabd2d
ridiyoga <44762924+ridiyoga@users.noreply.github.com>
2019-03-21 15:20:14 +0700

    halo

---
+++ ci.3.E41170897_P2/tokobuah/application/libraries/malasngoding.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+class Malasngoding{
+ 
+	function nama_saya(){
+		echo "Nama saya adalah Ridi !";
+	}
+ 
+	function nama_kamu($nama){
+		echo "Dan Saya akan Menguasai ". $nama ." !";
+	}
+}
