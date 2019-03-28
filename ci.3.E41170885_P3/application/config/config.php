51ca010baf8eda9cfc10ca40d5b0b64ac4254e1a
Rangga Triana Putra <44762808+ranggatriana@users.noreply.github.com>
2019-03-28 16:00:54 +0700

    fix done
    done

--- ci.3.E41170885_P3/application/config/config.php
+++ ci.3.E41170885_P3/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/malasngoding/';
+$config['base_url'] = 'http://localhost/ci.3.E41170885_P3/';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'ci.3.E41170885_P3';
 
 /*
 |--------------------------------------------------------------------------
