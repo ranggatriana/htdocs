b65b5a057d87106568e3f92afcc75cea9ebc6f40
mochammadRozyAndreanSyah <44630729+mochammadRozyAndreanSyah@users.noreply.github.com>
2019-04-02 12:31:46 +0700

    session

--- ci.3.E41171101_P3/application/controllers/crud.php
+++ ci.3.E41171101_P3/application/controllers/crud.php
@@ -6,10 +6,14 @@ class Crud extends CI_Controller{
 		parent::__construct();		
 		$this->load->model('m_data');
                 $this->load->helper('url');
+                if($this->session->userdata('status') != "login"){
+			redirect(base_url("index.php/login"));
+		}
 	}
 
 	function index(){
 		$data['user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_admin');
 		$this->load->view('v_tampil',$data);
 	}
 	function tambah(){
