3d6f18592477c59941bbb0f984173aa5f24cc7ec
ridiyoga <44762924+ridiyoga@users.noreply.github.com>
2019-05-09 13:13:59 +0700

    kk

---
+++ sitampol/application/views/v_tampil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title>
+</head>
+<body>
+	<center><h1>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</h1></center>
+	<center><?php echo anchor('crud/tambah','Tambah Data'); ?></center>
+	<table style="margin:20px auto;" border="1">
+		<tr>
+			<th>No</th>
+			<th>Nama Barang</th>
+			<th>Harga Barang</th>
+			<th>Stok</th>
+			<th>Kategori</th>
+		</tr>
+		<?php 
+		$no = 1;
+		foreach($user as $u){ 
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $u->nama_barang ?></td>
+			<td><?php echo $u->harga_barang ?></td>
+			<td><?php echo $u->stok ?></td>
+			<td><?php echo $u->id_kategori ?></td>
+			<td>
+			      <?php echo anchor('crud/edit/'.$u->id,'Edit'); ?>
+                              <?php echo anchor('crud/hapus/'.$u->id,'Hapus'); ?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+</body>
+</html>
